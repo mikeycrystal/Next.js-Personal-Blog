260f2aad1438b8905db06f82d928329995bd513e
mikeycrystal <michaelcrystal87@gmail.com>
2022-11-10 18:08:15 -0600

    Change File Structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "/Users/michaelcrystal/Desktop/nextjs-blog/styles/global.css";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
